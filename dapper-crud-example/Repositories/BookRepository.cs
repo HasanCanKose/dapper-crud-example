2a33084966795bce1962a3addd84a951d6530b32
Hasan Can Kose <hasancannkosee@gmail.com>
2021-06-13 06:49:56 +0300

    Add repository interface and implement it to book repository

---
+++ dapper-crud-example/Repositories/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using dapper_crud_example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_crud_example.Repositories
+{
+    public class BookRepository : IRepository<Book>
+    {
+        public void Add(Book t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dapper-crud-example/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_crud_example.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Add(T t);
+        List<T> GetAll();
+        T GetById(int id);
+        void Delete(int id);
+        void Update(T t);
+    
+    }
+}
