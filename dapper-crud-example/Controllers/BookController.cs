f5df6db5877d56742c4663e05e9f18e37f914689
Hasan Can Kose <hasancannkosee@gmail.com>
2021-06-13 07:10:10 +0300

    Add get request to get all books

---
+++ dapper-crud-example/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using dapper_crud_example.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_crud_example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookRepository bookRepository;
+
+        public BookController(BookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            return Ok(bookRepository.GetAll());
+        }
+    }
+}
