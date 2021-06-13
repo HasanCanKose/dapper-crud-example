83e1be292a5e78cc2261043d4c0f7392ee0eb783
Hasan Can Kose <hasancannkosee@gmail.com>
2021-06-13 06:38:03 +0300

    Add book model

---
+++ dapper-crud-example/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dapper_crud_example.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Plot { get; set; }
+        public string Author { get; set; }
+    }
+}
